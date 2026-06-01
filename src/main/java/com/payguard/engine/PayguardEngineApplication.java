280204be118e7852642be5c09108b84a5ff0aadc
StockFish-m <nguyenhoang500235@gmail.com>
2026-06-01 22:33:49 +0700

    initial commit

---
+++ src/main/java/com/payguard/engine/PayguardEngineApplication.java
@@ -0,0 +1,13 @@
+package com.payguard.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayguardEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayguardEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payguard/engine/PayguardEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payguard.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayguardEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
